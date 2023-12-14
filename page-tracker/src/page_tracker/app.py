c759337f00cd82dfce50be7fe5c202512f5658b5
Mathias Bensimhon <mathiasbensimhon@Mathiass-MacBook-Pro.local>
2023-12-14 12:41:05 +0200

    working on the Dockerfile

--- page-tracker/src/page_tracker/app.py
+++ page-tracker/src/page_tracker/app.py
@@ -1,5 +1,6 @@
 # src/page-tracker/app.py
 
+import os
 from functools import cache
 
 from flask import Flask
@@ -7,6 +8,7 @@ from redis import Redis, RedisError
 
 app = Flask(__name__)
 # redis = Redis()
+
 
 @app.get("/")
 def index():
@@ -17,8 +19,9 @@ def index():
         app.logger.exception("Redis error")
         return "Sorry, something went wrong \N{pensive face}", 500
     else:
-        return f'this page has been seen {page_views} times.'
+        return f"this page has been seen {page_views} times."
+
 
 @cache
 def redis():
-    return Redis()
+    return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
